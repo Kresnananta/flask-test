3c4e48c231eb03f100f036bce940fb6c46a70ac7
Kresnananta <akresnaananta@gmail.com>
2026-02-01 19:53:05 +0800

    change groq model

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import os
+
+from groq import Groq
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 from datetime import datetime
 
+load_dotenv()
+
 app = Flask(__name__)
+
+API_KEY = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key=API_KEY)
+
+def ai_call(year):
+    try:
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user", 
+                    "content": f"berikan saya fakta menarik tentang tahun {year} dalam 2-3 kalimat",
+                }
+            ],
+            model="llama-3.1-8b-instant",
+            stream=False
+        )
+
+        ai_output = chat_completion.choices[0].message.content
+        return ai_output
+    except Exception:
+        return "Error, gagal mendapatkan data dari AI."
+
 
 @app.route('/')
 def main():
@@ -14,7 +43,11 @@ def cek_usia():
         tahun_lahir = int(request.form['tahun_lahir'])
         tahun_sekarang = datetime.now().year
         usia = tahun_sekarang - tahun_lahir
-        return render_template('cek_usia.html', usia=usia, tahun_lahir=tahun_lahir)
+
+        ai_output = ai_call(tahun_lahir)
+        #print(ai_output)
+
+        return render_template('cek_usia.html', usia=usia, tahun_lahir=tahun_lahir, ai_output=ai_output)
     return render_template('cek_usia.html', usia= None)
     
 if __name__ == "__main__":
